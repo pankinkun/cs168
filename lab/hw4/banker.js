27f85838bdbf7e696f95cbd83b839d88d051af85
Trique Nguyen <ngngoctridung123@gmail.com>
2024-04-25 15:24:54 -0700

    hw4

---
+++ lab/hw4/banker.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const { Client } = require('spartan-gold');
+
+const SpartanPyrite = require('./spartan-pyrite');
+
+/**
+ * The banker is a client responsible for paying out other clients
+ * once they have burned their SpartanPyrite tokens.
+ */
+module.exports = class Banker extends Client {
+
+  /**
+   * The Banker needs the address of the SpartanPyrite contract,
+   * in addition to the other configuration details for clients.
+   */
+  constructor(cfg) {
+    super(cfg);
+    this.sp = new SpartanPyrite({ contractAddress: cfg.contractAddress });
+    this.burned = new Map();
+  }
+
+  /**
+   * When called, the banker checks the SpartanPyrite smart contract to see how
+   * many total coins have been burned by this Ethereum address and writes a
+   * transaction to pay out an equal amount of gold.
+   * 
+   * IMPORTANT NOTE: This method should only pay out the **difference** between
+   * the total amount of tokens burned and the amount of gold already given to
+   * the token burner.
+   * 
+   * @param {String} ethAddress - Ethereum address that has burned SPYR tokens.
+   */
+  mintGold(ethAddress) {
+    //
+    // **YOUR CODE HERE**
+    //
+  }
+
+}
